af143ee41457b22740ec8ee535917f94fbb4c4f6
Ali Lorenzo <ajosue998@gmail.com>
2022-08-20 17:29:38 +0200

    Add: functionality to correct the word

--- app.js
+++ app.js
@@ -1,44 +1,85 @@
-var numberFila= 1;
-var numberColumna =0;
+var numberRow = 1;
+var numberColumn = 0;
+var correctLetters = 0;
+var isLoading = false;
+var WORD = 'cortos'
+var WORD_ARRAY = WORD.split('');
 
-async function getWord(){
-  fetch('https://palabras-aleatorias-public-api.herokuapp.com/random')
-  .then(response => response.json())
-  .then(data => {console.log (data)})
+function validateGoodLetters() {
+  for (let i = 1; i <= 6; i++) {
+    let parrafo = document.getElementById(`${numberColumn}${i}`);
+    let letra = WORD_ARRAY[i - 1];
+    if (parrafo.innerHTML === letra.toUpperCase()) {
+
+      let elemento = document.getElementById(`box${numberColumn}${i}`);
+      elemento.className += " correct";
+      correctLetters++;
+      WORD_ARRAY[i - 1] = ''
+    }
+  }
+}
+
+function validateWord() {
+  for (let i = 1; i <= 6; i++) {
+    let letter = document.getElementById(`${numberColumn}${i}`);
+
+    if (WORD_ARRAY.find(element => element === letter.innerHTML.toLowerCase()) && WORD_ARRAY[i - 1] !== '') {
+      let elemento = document.getElementById(`box${numberColumn}${i}`);
+      elemento.className += " cerca"
+    } else if (WORD_ARRAY[i - 1] !== '') {
+      let elemento = document.getElementById(`box${numberColumn}${i}`);
+      elemento.className += " incorrect";
+    }
+  }
+
+}
+
+async function getWord() {
+  fetch('https://palabras-aleatorias-public-api.herokuapp.com/random-by-length?length=6')
+    .then(response => response.json())
+    .then(data => {
+      /*PALABRA = data.body.Word;
+      console.log(data.body.Word);
+      PALABRA_ARRAY = PALABRA.split('');*/
+      isLoading = false;
+    })
 }
 
 document.addEventListener('keydown', (event) => {
-  
+
   var keyValue = event.key;
-
-  if(allowOnlyAlphabets(event) && numberFila <= 6){    
-    document.getElementById(`${numberColumna}${numberFila}`).innerHTML=keyValue.toUpperCase();
-    numberFila++;
+  if (event.key === 'Enter' && numberRow === 7 && isLoading === false) {
+    numberRow = 1;
+    validateGoodLetters();
+    validateWord();
+    numberColumn++;
+    correctLetters = 0
+    WORD_ARRAY = WORD.split('');
   }
 
-  if(event.key === 'Backspace' && numberFila >= 1){    
-    console.log(numberFila)
-    document.getElementById(`${numberColumna}${numberFila}`).innerHTML='';
-    numberFila--;
+
+  if (allowOnlyAlphabets(event) && numberRow <= 7) {
+    document.getElementById(`${numberColumn}${numberRow}`).innerHTML = keyValue.toUpperCase();
+    numberRow++;
   }
 
-  if(event.key === 'Enter'){
-    numberFila= 1;
-    numberColumna++;
+  if (event.key === 'Backspace' && numberRow >= 1) {
+    document.getElementById(`${numberColumn}${numberRow - 1}`).innerHTML = '';
+    numberRow--;
   }
 
-  if(numberFila>6){
-    numberFila=6;
+
+  if (numberRow > 7) {
+    numberRow = 7;
   }
-  if(numberFila===0){
-    numberFila=1;
+  if (numberRow === 0) {
+    numberRow = 1;
   }
-  
+
 }, false);
 
 function allowOnlyAlphabets(event) {
   var charCode = event.keyCode;
- 
   if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123))
     return true;
   else
